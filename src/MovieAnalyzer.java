21a6277ae9262bfd6e086d79e59be702d50f04d3
jr <1975863521@qq.com>
2022-10-12 22:49:18 +0800

    read

--- src/MovieAnalyzer.java
+++ src/MovieAnalyzer.java
@@ -2,8 +2,10 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class MovieAnalyzer {
+    String Poster_Link;
     String Series_Title;// Name of the movie
     String Released_Year;// Year at which that movie released
     String Certificate;// Certificate earned by that movie
@@ -19,6 +21,57 @@ public class MovieAnalyzer {
 
     public MovieAnalyzer(String dataset_path) {
         String[] s = dataset_path.split(",");
+        int flag = 0, index = 0;
+        String now = null;
+        for (String ss : s) {
+            if (ss.charAt(0) == '\"' || ss.charAt(ss.length() - 1) == '\"') {
+                flag++;
+                flag %= 2;
+            }
+            now += ss;
+            if (flag == 0) {
+                switch (index) {
+                    case 0 : this.Poster_Link = now;
+                    case 1 : this.Series_Title = now;
+                    case 2 : this.Released_Year = now;
+                    case 3 : this.Certificate = now;
+                    case 4 : this.Runtime = now;
+                    case 5 : this.Genre = now;
+                    case 6 : this.IMDB_Rating = now;
+                    case 7 : this.Overview = now;
+                    case 8 : this.Meta_score = now;
+                    case 9 : this.Director = now;
+                    case 10 : this.Star1 = now;
+                    case 11 : this.Star2 = now;
+                    case 12 : this.Star3 = now;
+                    case 13 : this.Star4 = now;
+                    case 14 : this.Noofvotes = now;
+                    case 15 : this.Gross = now;
+                }
+                index++;
+                now = null;
+            } else {
+                now += ",";
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        File file = new File("D:\\study\\G3\\Java2\\Assignment\\A1_Sample\\resources\\imdb_top_500.csv");
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String tempString;
+            int line = 1;
+            // 一次读入一行，直到读入null为文件结束
+            while ((tempString = reader.readLine()) != null) {
+                // 显示行号
+                // System.out.println("line " + line + ": " + tempString);
+                MovieAnalyzer movieAnalyzer = new MovieAnalyzer(tempString);
+                line++;
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
